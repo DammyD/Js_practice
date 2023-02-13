77e534c26311421849cf4f7c1d836e4f2f0c4278
DammyD <ibukunoluwadawodu@gmail.com>
2023-02-13 13:14:29 +0100

    :zap: variable.js file

---
+++ variables.js
@@ -0,0 +1,52 @@
+/**
+ * Class Teachings
+ * 
+ * Variables are likened to containers for storing values
+ * My name is Blessing
+ * I am a Frontend Engineer
+ * I live in Ogun State, Nigeria
+ * 
+ * let firstName = 'Blessing';
+ * let profession ='Frontend Engineer';
+ * let location = 'Ogun State, Nigeria';
+
+console.log(firstName);
+console.log(profession);
+console.log(location);
+
+console.log("My name is " + firstName +". I am a " + profession +". I live in " + location + ".");
+
+let age = 23;
+console.log("I am " + age + " years old.");
+console.log("54" + 5);
+console.log(73 + "5");
+console.log("65" - 6);
+console.log(34 + 6);
+// JS is a dynamically typed or weakly typed language: Meaning the type of the variable is determined at the time of execution
+// It can change a type based on the value assigned to it
+
+// Three keywords used to decribe/declare a variable
+// var - almost out of phase
+// let -
+// const
+
+// what constitute a variable name: Varaible name can be constructed using the following
+// A-Z, a-z
+// underscore (_)
+// Numbers 0-9
+ Variable names are case sensitive
+
+* There are 8 datatypes in JavaScript
+* Primitive Datatypes
+* - Number
+* - String
+* - Boolean (true/false)
+* - Null
+* - Undefined
+* - BigInt (large numbers)
+* - Symbol
+* - Object
+* */
+
+let y = null;
+console.log(y);
