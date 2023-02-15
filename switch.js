a5ef5519bc58a7e02ab0a076925a56ac6198bbf7
DammyD <ibukunoluwadawodu@gmail.com>
2023-02-15 13:09:52 +0100

    :rocket: switch cases in JavaScript

---
+++ switch.js
@@ -0,0 +1,21 @@
+// Swicth Statement - Is used in decision making and when you want to use equality operator
+//  Science Subjects- Physics, Chemistry, Biology, Technical Drawing, English, Mathematics
+// Social Science Subjects - Accounting, Geology, commerece, Marketing, English, Mathematics
+// Arts Subjects - Government, Economics, LIterature, History, English, Mathematics
+
+//Assignment 1: Rewrite this using an if..else statement
+let studentGroup = 'SOCIAL SCIENCES';
+switch (studentGroup) {
+    case 'SCIENCE':
+        console.log('Physics, Chemistry, Biology, Technical Drawing')
+        break;
+    case 'SOCIAL SCIENCES':
+        console.log('Accounting, Geology, Commerce, Marketing')
+        break;
+    case 'ARTS':
+        console.log('Government, Economics, LIterature, History')
+        break;
+    default:
+        console.log('English, Mathematics')
+        break;
+}
